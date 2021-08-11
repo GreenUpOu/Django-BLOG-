e97bb8d2dac775032c7e7e3ad6ae2a348bc08bc3
GreenUpOu <27062015grisha@gmail.com>
2021-08-11 18:06:47 +0300

    test, CRUD posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name = 'home'),
-    path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post_detail'),
+    path('post/<int:pk>/',
+         views.BlogDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'post_new'),
+    path('post/<int:pk>/edit/',
+         views.BLogUpdateView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/delete/',
+         views.BLogDeleteView.as_view(), name = 'post_delete'),
 ]
